5d3a0a7953aebc8758db1753a2e62999744d01c7
Julian Scharf <jscharf@thoughtworks.com>
2018-02-14 15:51:28 +1000

    refactored 2/3 of the methods internal to the sanitize user

--- index.js
+++ index.js
@@ -3,109 +3,101 @@
  * See problem 0 in the spec file for the assertion
  * @returns {string}
  */
-exports.example = () => {
-    return 'hello world';
-};
+exports.example = () => 'hello world';
 
 exports.stripPrivateProperties = (secrets, people) => {
-    var sanitizedPeople = [];
-    people.forEach(person => {
-        var sanitizedPerson = {};
-        for (var key in person) {
-            if ((person.hasOwnProperty(key)) && (!secrets.includes(key))) {
-                sanitizedPerson[key] = person[key];
-            }
-        }
-        sanitizedPeople.push(sanitizedPerson);
-    });
-    return sanitizedPeople;
+  const sanitizedPeople = [];
+  people.forEach((person) => {
+    const sanitizedPerson = {};
+    for (const key in person) {
+      if ((person.hasOwnProperty(key)) && (!secrets.includes(key))) {
+        sanitizedPerson[key] = person[key];
+      }
+    }
+    sanitizedPeople.push(sanitizedPerson);
+  });
+  return sanitizedPeople;
 };
 
-exports.excludeByProperty = (property, payload) => {
-    return payload.filter(entry => !entry.hasOwnProperty(property))
-};
+exports.excludeByProperty = (property, payload) => payload.filter(entry => !entry.hasOwnProperty(property));
 
 exports.sumDeep = (payload) => {
-    const reducer = (accum, element) => accum + element.val;
-    const sumTheObject = (element) => element.objects.reduce(reducer, 0);
-    return payload.map(sumTheObject).map(sum => ({ objects: sum }));
+  const reducer = (accum, element) => accum + element.val;
+  const sumTheObject = element => element.objects.reduce(reducer, 0);
+  return payload.map(sumTheObject).map(sum => ({ objects: sum }));
 };
 
 exports.applyStatusColor = (colors, statuses) => {
-
-    var colorLookup = {};
-    function populateColorLookup(color, statuses, colorLookup) {
-        statuses.forEach((code) => {
-            colorLookup[code] = color;
-        })
-    }
-
-    Object.entries(colors).forEach(
-        ([color, codeArray]) => {
-            populateColorLookup(color, codeArray, colorLookup);
-        }
-    );
-
-    const validStatuses = statuses.filter(status => (colorLookup.hasOwnProperty(status['status'])));
-
-     return validStatuses.map(element => {
-        const status = element['status'];
-        const color = (colorLookup[status]);
-        return {
-            'color': color,
-            'status': status
-        }
+  const colorLookup = {};
+  function populateColorLookup(color, statuses, colorLookup) {
+    statuses.forEach((code) => {
+      colorLookup[code] = color;
     });
+  }
+
+  Object.entries(colors).forEach(([color, codeArray]) => {
+    populateColorLookup(color, codeArray, colorLookup);
+  });
+
+  const validStatuses = statuses.filter(status => (colorLookup.hasOwnProperty(status.status)));
+
+  return validStatuses.map((element) => {
+    const status = element.status;
+    const color = (colorLookup[status]);
+    return {
+      color,
+      status,
+    };
+  });
 };
 
-exports.createGreeting = (greeterFunc, phrase) => {
-    return (name) => (phrase + ' ' + name);
-};
+exports.createGreeting = (greeterFunc, phrase) => name => (`${phrase} ${name}`);
 
 exports.setDefaults = (defaultProps) => {
-    const applyDefaults = (user) => {
-        const userKeys = Object.keys(user);
-        const defaultKeys = Object.keys(defaultProps);
-        const checkForMissingKeys = (defaultKey => !userKeys.includes(defaultKey));
-        const missingDefaultKeys = defaultKeys.filter(checkForMissingKeys);
+  const applyDefaults = (user) => {
+    const userKeys = Object.keys(user);
+    const defaultKeys = Object.keys(defaultProps);
+    const checkForMissingKeys = (defaultKey => !userKeys.includes(defaultKey));
+    const missingDefaultKeys = defaultKeys.filter(checkForMissingKeys);
 
-        var newUser = user;
-        missingDefaultKeys.forEach((key) => newUser[key] = defaultProps[key]);
-        return newUser;
-    };
-    return applyDefaults;
+    const newUser = user;
+    missingDefaultKeys.forEach(key => newUser[key] = defaultProps[key]);
+    return newUser;
+  };
+  return applyDefaults;
 };
 
 exports.sanitizeUser = (user) => {
-    const log = (msg) => (console.log(msg)); 
+  const newUser = user;
+  const log = msg => (console.log(msg));
+  const isString = element => (Object.prototype.toString.call(element) === '[object String]');
 
-    var foundUsersFirstname;
-    
-    const isNumber = (element) => (Object.prototype.toString.call(element) === `[object Number]`);
-    const isString = (element) => (Object.prototype.toString.call(element) === `[object String]`);
-
-    if (isString(user.name)) {
-
-    } else {
-        log('User name is not a valid String');
-        return '';
+  const getFirstName = (fullName) => {
+    if (isString(fullName)) {
+      return fullName.split(' ')[0];
     }
+    log('User name is not a valid String');
+    return '';
+  };
 
-    // Ensure a user has an `fullAddress` property by combining `address.streetNum, address.streetName, address.suburb`
-    if (user.address.num && user.address.street && user.address.suburb) {
-        user.fullAddress = user.address.num + ' ' + user.address.street + ', ' + user.address.suburb;
+  newUser.firstName = getFirstName(newUser.name);
+
+  if (newUser.address.num && newUser.address.street && newUser.address.suburb) {
+    newUser.fullAddress = `${newUser.address.num} ${newUser.address.street}, ${newUser.address.suburb}`;
+  }
+
+  const getMonthJoined = (month) => {
+    const validMonths = [...Array(12).keys()];
+    if (validMonths.includes(parseInt(month, 10))) {
+      return month + 1;
     }
+    log('invalid monthJoined: wrong type or out of range');
+    return month;
+  };
 
-    // The given user always returns the `monthJoined` as 0 to 11. We need it to be 1 to 12 so add 1.
-    if (user.monthJoined)
-    user.monthJoined = user.monthJoined + 1;
+  if (newUser.monthJoined) {
+    newUser.monthJoined = getMonthJoined(newUser.monthJoined);
+  }
 
-
-    user.firstName = getNameArray(user.name)[0];
-    
-
-    return user;
+  return newUser;
 };
-
-
-
